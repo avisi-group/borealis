ae4c163af9fe5412a28cc347b09bbc7e0234fab4
Ferdia McKeogh <ferdia@mckeogh.tech>
2022-11-09 14:02:25 +0000

    Continue formatting implementation

--- borealis/src/main.rs
+++ borealis/src/main.rs
@@ -1,10 +1,8 @@
-use borealis::genc::{Bank, RegisterSpace};
-
 use {
-    borealis::genc::{self},
+    borealis::genc::Description,
     clap::Parser,
     color_eyre::eyre::{Result, WrapErr},
-    std::{collections::HashMap, path::PathBuf},
+    std::path::PathBuf,
 };
 
 #[derive(Parser, Debug)]
@@ -24,29 +22,9 @@ fn main() -> Result<()> {
 
     // parse command line arguments
     let args = Args::parse();
-
-    genc::Description {
-        name: "arm64".to_owned(),
-        endianness: genc::Endianness::LittleEndian,
-        wordsize: 64,
-        fetchsize: 32,
-        registers: vec![RegisterSpace {
-            size: 248,
-            banks: vec![Bank {
-                name: "RBX".to_owned(),
-                typ: genc::Typ::Uint64,
-                offset: 0,
-                count: 31,
-                stride: 8,
-                element_count: 1,
-                element_size: 8,
-                element_stride: 8,
-            }],
-        }],
-        instructions: HashMap::new(),
-    }
-    .export(args.out_dir, args.force)
-    .wrap_err("Error while exporting GenC description")?;
+    Description::empty()
+        .export(args.out_dir, args.force)
+        .wrap_err("Error while exporting GenC description")?;
 
     Ok(())
 }
