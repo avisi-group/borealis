4bf2161b01eebddff780817b24cd437eb02c5e4d
Ferdia McKeogh <ferdia@mckeogh.tech>
2022-12-07 11:27:08 +0000

    Remove examples and add multiple format support to main `borealis` binary

--- borealis/src/main.rs
+++ borealis/src/main.rs
@@ -1,10 +1,13 @@
 use {
     borealis::genc::{export, Description},
-    clap::Parser,
-    color_eyre::eyre::{Result, WrapErr},
+    clap::{Parser, Subcommand},
+    color_eyre::eyre::{bail, Result, WrapErr},
     deepsize::DeepSizeOf,
+    sail::dot,
     std::{
-        io::{self, Write},
+        ffi::OsStr,
+        fs::File,
+        io::{self, BufReader, Write},
         path::PathBuf,
     },
 };
@@ -12,17 +15,28 @@ use {
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Path to Sail JSON config
+    /// Path to Sail JSON config or bincode AST
     #[arg(short)]
     input: PathBuf,
 
-    /// Path to empty folder where GenC description files will be emitted.
-    #[arg(short)]
-    output: PathBuf,
+    #[command(subcommand)]
+    output: Output,
+}
 
-    #[arg(long)]
-    /// Warning! Disables checking that output directory is empty before writing files.
-    force: bool,
+#[derive(Subcommand, Debug)]
+enum Output {
+    Genc {
+        /// Path to empty folder where GenC description files will be emitted.
+        #[arg(short)]
+        output: PathBuf,
+
+        #[arg(long)]
+        /// Warning! Disables checking that output directory is empty before writing files.
+        force: bool,
+    },
+    Dot,
+    Json,
+    Bincode,
 }
 
 fn main() -> Result<()> {
@@ -31,11 +45,20 @@ fn main() -> Result<()> {
     // parse command line arguments
     let args = Args::parse();
 
-    let (_name, ast, _env) =
-        sail::load_from_config(args.input).wrap_err("Failed to load Sail files")?;
-
-    export(&Description::empty(), args.output, args.force)
-        .wrap_err("Error while exporting GenC description")?;
+    // either parse AST from Sail config file or deserialize AST from bincode
+    let ast = match args.input.extension().and_then(OsStr::to_str) {
+        Some("json") => {
+            sail::load_from_config(args.input)
+                .wrap_err("Failed to load Sail files")?
+                .1
+        }
+        Some("bincode") => {
+            let file = File::open(args.input)?;
+            let reader = BufReader::new(file);
+            bincode::deserialize_from(reader)?
+        }
+        _ => bail!("Unrecognised input format {:?}", args.input),
+    };
 
     writeln!(
         io::stderr().lock(),
@@ -58,7 +81,13 @@ fn main() -> Result<()> {
         common::identifiable::unique_id()
     )?;
 
-    bincode::serialize_into(io::stdout().lock(), &ast)?;
+    match args.output {
+        Output::Genc { output, force } => export(&Description::empty(), output, force)
+            .wrap_err("Error while exporting GenC description")?,
+        Output::Dot => dot::render(&ast, &mut io::stdout().lock())?,
+        Output::Json => serde_json::to_writer_pretty(io::stdout().lock(), &ast)?,
+        Output::Bincode => bincode::serialize_into(io::stdout().lock(), &ast)?,
+    }
 
     Ok(())
 }
