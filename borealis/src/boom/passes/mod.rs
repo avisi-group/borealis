b8cca80e75c1d02fb426a4d525b816284052b4ff
Ferdia McKeogh <ferdia@mckeogh.tech>
2023-05-09 10:07:47 +0100

    Dump single function control flow
    Former-commit-id: b6cb64987033556fe4bcca183461734712cee3d0
    Former-commit-id: 956c6216ea6c6cb82fb9775fb87544b170d865ac

--- borealis/src/boom/passes/mod.rs
+++ borealis/src/boom/passes/mod.rs
@@ -13,7 +13,12 @@ use {
         Ast,
     },
     log::info,
-    std::{cell::RefCell, rc::Rc},
+    std::{
+        cell::RefCell,
+        fs::{create_dir_all, File},
+        path::Path,
+        rc::Rc,
+    },
 };
 
 mod builtin_fns;
@@ -31,7 +36,14 @@ pub fn execute_passes(ast: Rc<RefCell<Ast>>) {
     .into_iter()
     .for_each(|mut pass| {
         info!("{}", pass.name());
-        pass.run(ast.clone())
+
+        pass.run(ast.clone());
+
+        dump_func_dot(
+            &ast,
+            "integer_arithmetic_addsub_immediate_decode",
+            format!("target/dot/addsub_{}.dot", pass.name()),
+        );
     });
 }
 
@@ -40,4 +52,18 @@ pub trait Pass {
 
     /// Run the pass on the supplied AST
     fn run(&mut self, ast: Rc<RefCell<Ast>>);
+}
+
+fn dump_func_dot<P: AsRef<Path>>(ast: &Rc<RefCell<Ast>>, func: &'static str, path: P) {
+    if let Some(parent) = path.as_ref().parent() {
+        create_dir_all(parent).unwrap();
+    }
+
+    ast.borrow()
+        .functions
+        .get(&func.into())
+        .unwrap()
+        .control_flow
+        .as_dot(&mut File::create(path).unwrap())
+        .unwrap()
 }
