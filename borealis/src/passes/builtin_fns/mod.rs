82c48676fa3d6032ec8cdc92af232d99b5b5cff0
Ferdia McKeogh <ferdia@mckeogh.tech>
2023-07-22 09:38:56 +0100

    Use HashMap for builtin function handlers, initial work on `slice` handler

--- borealis/src/passes/builtin_fns/mod.rs
+++ borealis/src/passes/builtin_fns/mod.rs
@@ -4,12 +4,25 @@
 use {
     crate::{
         boom::{Ast, FunctionDefinition, Statement},
-        passes::Pass,
+        passes::{builtin_fns::functions::HANDLERS, Pass},
     },
-    std::{cell::RefCell, rc::Rc},
+    common::{intern::InternedString, HashMap, HashSet},
+    once_cell::sync::Lazy,
+    std::{cell::RefCell, hash::Hash, rc::Rc},
 };
 
 pub mod functions;
+
+type HandlerFunction = fn(Rc<RefCell<Ast>>, FunctionDefinition, Rc<RefCell<Statement>>);
+
+/// Functions to handle in an initial pass, before processing others
+const INITIAL_PASS_FNS: Lazy<HashSet<InternedString>> = Lazy::new(|| {
+    HashSet::from_iter(
+        ["UInt", "pcnt_i___pcnt_i64", "pcnt_i64___pcnt_i"]
+            .into_iter()
+            .map(InternedString::from_static),
+    )
+});
 
 pub struct AddBuiltinFns;
 
@@ -30,16 +43,26 @@ impl Pass for AddBuiltinFns {
         // otherwise, lookup function in functions and execute behaviour (either in
         // place modification or inserting new function definition)
 
+        // perform initial pass on limited set of functions
+        ast.borrow().functions.values().for_each(|def| {
+            process_function_definition(ast.clone(), def, &difference(&HANDLERS, &INITIAL_PASS_FNS))
+        });
+
+        // perform full pass on all functions
         ast.borrow()
             .functions
             .values()
-            .for_each(|def| process_function_definition(ast.clone(), def));
+            .for_each(|def| process_function_definition(ast.clone(), def, &HANDLERS));
 
         false
     }
 }
 
-fn process_function_definition(ast: Rc<RefCell<Ast>>, fn_def: &FunctionDefinition) {
+fn process_function_definition(
+    ast: Rc<RefCell<Ast>>,
+    fn_def: &FunctionDefinition,
+    handlers: &HashMap<InternedString, HandlerFunction>,
+) {
     fn_def.entry_block.iter().for_each(|block| {
         block.statements().into_iter().for_each(|statement| {
             // ignore statements that are not function calls
@@ -52,14 +75,21 @@ fn process_function_definition(ast: Rc<RefCell<Ast>>, fn_def: &FunctionDefinitio
                 return;
             }
 
-            // found builtin function function
-            let handler = functions::HANDLERS.get(name.as_ref()).unwrap_or_else(|| {
-                panic!(
-                    "Function call {name:?} found without definition or builtin function behaviour"
-                )
-            });
-
-            handler(ast.clone(), fn_def.clone(), statement);
+            // if the function has a handler, call it
+            if let Some(handler) = handlers.get(&name) {
+                handler(ast.clone(), fn_def.clone(), statement);
+            }
         });
     });
+}
+
+/// Returns the key-value pairs in `map` where the keys exist in `set`
+fn difference<K: Eq + PartialEq + Hash + Copy, V: Clone>(
+    map: &HashMap<K, V>,
+    set: &HashSet<K>,
+) -> HashMap<K, V> {
+    map.into_iter()
+        .filter(|(k, _)| set.contains(k))
+        .map(|(&k, v)| (k, v.clone()))
+        .collect()
 }
